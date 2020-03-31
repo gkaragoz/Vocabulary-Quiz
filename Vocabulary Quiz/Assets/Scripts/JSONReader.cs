2a80e0bc3d28b2e0bcf3a74f0b3b7b1d922062a7
gkaragoz <gkaragoz96@gmail.com>
2020-03-31 23:33:07 +0300

    Create JSONReader.cs script for test purposes.

---
+++ Vocabulary Quiz/Assets/Scripts/JSONReader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class JSONReader : MonoBehaviour {
+
+    public Word[] words;
+    public Word testWord;
+
+    private void Start() {
+        Word word = new Word() {
+            en = "Pencil",
+            tr = "Kalem"
+        };
+
+        string jsonString = JsonUtility.ToJson(word);
+        Debug.Log(jsonString);
+
+        Word convertedFromJson = JsonUtility.FromJson<Word>(jsonString);
+    }
+
+}
